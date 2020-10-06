0d742df0153e3a5bc0bb257877bd5b0abddad52e
Arthur Nunes <arthurnunes@tutanota.com>
2020-10-06 07:54:30 -0300

    Add -y to Alacritty's dnf copr

--- script.sh
+++ script.sh
@@ -155,7 +155,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # Add Alacritty repo
-dnf copr enable pschyska/alacritty
+dnf copr enable pschyska/alacritty -y
 
 # Install Node
 curl -sL https://rpm.nodesource.com/setup_14.x | bash -
