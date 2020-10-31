cc5000729f606979a68c23816f651b1497667dca
Arthur Nunes <arthurnunes@tutanota.com>
2020-10-31 15:32:31 -0300

    Minor refactoring

--- components/clone_github_projects.sh
+++ components/clone_github_projects.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 github_projects_links=(
-  https://github.com/arthurnunesc/arthurnunesc.github.io.git
   https://github.com/arthurnunesc/postinstall-fedora.git
-  https://github.com/arthurnunesc/postinstall-openSUSE.git
+  https://github.com/arthurnunesc/arthurnunesc.github.io.git
+  https://github.com/arthurnunesc/blog-posts.git
   https://github.com/arthurnunesc/ansible.git
 )
 
 if [ ! -d "$HOME/Projects" ]; then
   mkdir $HOME/Projects
 fi
+cd $HOME/Projects
 for link in "${github_projects_links[@]}"; do
-  git clone $link $HOME/Projects
+  git clone $link
 done
+cd
--- script.sh
+++ script.sh
@@ -137,11 +137,11 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install code -y -q
 
-# Install Node
+# Install Node + NPM
 curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 
-# Install Rust's Cargo
-curl https://sh.rustup.rs -sSf | sh
+# Install Rust + Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 update_repos_and_apps
 
