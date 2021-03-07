3de894050f86ff359efa7164cfb6fa1eb881f720
Arthur Nunes <arthurnunes@tutanota.com>
2021-03-07 17:22:49 -0300

    Add install fonts component

---
+++ components/install_fonts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp ~/Dropbox/Customizações e Apps/Fira Sans + Fira Code + JetBrains Mono/*.ttf ~/.local/share/fonts
+cp ~/Dropbox/Customizações e Apps/Fira Sans + Fira Code + JetBrains Mono/*.otf ~/.local/share/fonts
