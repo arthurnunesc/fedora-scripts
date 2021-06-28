7b3769e604cd265f3b4066c9a87e2a4ea62fd14b
Arthur Nunes <arthurnunescorrea@gmail.com>
2021-06-28 18:01:26 -0300

    Update script.sh
    add and delete some packages

--- script.sh
+++ script.sh
@@ -17,17 +17,13 @@ dnf_apps=(
   mozilla-fira-sans-fonts
   mozilla-fira-mono-fonts
   fira-code-fonts
-  nautilus-dropbox
   https://release.axocdn.com/linux/gitkraken-amd64.rpm # GitKraken
   winehq-staging
-  gcc-c++ # NodeJS build tools
-  ansible
-  python-psutil # Ansible dconf dependency
-)
-dnf_apps_desktop_only=(
   piper
   lutris
   steam
+)
+dnf_apps_desktop_only=(
 )
 
 flatpak_apps=(
@@ -133,9 +129,6 @@ sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/32/w
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install code -y -q
-
-# Install Node + NPM
-curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 
 update_repos_and_apps
 
