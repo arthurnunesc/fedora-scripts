09b5db16c2118801bd39d39ee758e78ac2a67aee
Arthur Nunes <arthurnunesc@proton.me>
2023-04-10 22:38:58 +0200

    Add condition to black venv creation

--- components/python.sh
+++ components/python.sh
@@ -2,12 +2,15 @@
 
 pip_apps="pip setuptools tldr norminette ruff-lsp black pynvim black[jupyter] black[d]"
 
-mkdir -p ~/.local/venv && cd ~/.local/venv || return
-python3 -m venv nvim
-cd nvim || return
-. ./bin/activate
-pip install pynvim black
-cd || return
+if [ ! -d "$HOME"/.local/venv/nvim ]; then
+  mkdir -p "$HOME"/.local/venv && cd "$HOME"/.local/venv || return
+  python3 -m venv nvim
+  cd nvim || return
+  . ./bin/activate
+  pip install -Uq pynvim black
+  cd || return
+fi
+
 
 echo "$pip_apps" | tr ' ' '\n' | while read -r item; do
   pip install -Uq "$item"
