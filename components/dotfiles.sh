71ab097038b7c98ff9a401a5e3a5d92b13025f09
Arthur Nunes <arthurnunesc@proton.me>
2023-04-19 23:26:49 +0200

    Delete some more macOS things and zsh plugins

--- components/dotfiles.sh
+++ components/dotfiles.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env dash
 
-# Clones zsh plugins into .config
-sh ./components/repo_updater.sh "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.config/zsh/plugins/zsh-syntax-highlighting" "master"
-sh ./components/repo_updater.sh "https://github.com/zsh-users/zsh-autosuggestions.git" "$HOME/.config/zsh/plugins/zsh-autosuggestions" "master"
-sh ./components/repo_updater.sh "https://github.com/zsh-users/zsh-completions.git" "$HOME/.config/zsh/plugins/zsh-completions" "master"
-
 # Clones dotfiles from my GitHub repo
 sh ./components/repo_updater.sh "https://github.com/arthurnunesc/dotfiles.git" "$HOME/.dotfiles"
 
 rm -rf "$HOME"/.zshrc
 
 # Synchronizes all dotfiles with stow
-cd "$HOME"/.dotfiles || return
+cd "$HOME"/.dotfiles || exit 1
 stow */ -t "$HOME"
-cd || return
+cd - || exit 1
