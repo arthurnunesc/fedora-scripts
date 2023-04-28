e8e5227023b3cc779f564139098e0c1b2f016447
Arthur Nunes <arthurnunesc@proton.me>
2023-04-28 22:08:37 +0200

    Delete eyecandy.sh

--- components/eyecandy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Making sure fonts and icons folders exist to not make them be created as symbolic links and clone eyecandy repo
-if [ ! -d "$HOME"/.local/share/fonts ]; then
-  mkdir -p "$HOME"/.local/share/fonts
-fi
-sh ./components/repo_updater.sh "https://github.com/arthurnunesc/eyecandy.git" "$HOME/.local/share/fonts"
