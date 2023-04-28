cb3e0ab730157b7d220e369782ae40527dc346ac
Arthur Nunes <arthurnunesc@proton.me>
2023-04-28 22:08:29 +0200

    Migrate from GNU Stow to chezmoi

--- components/dotfiles.sh
+++ components/dotfiles.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env sh
 
+# Delete default .zshrc file
 rm -rf "$HOME"/.zshrc
 
 # Clones dotfiles from my GitHub repo
-sh ./components/repo_updater.sh "https://github.com/arthurnunesc/dotfiles.git" "$HOME/.dotfiles"
-
-# Synchronizes all dotfiles with stow
-cd "$HOME"/.dotfiles || exit 1
-stow */ --target="$HOME"
-cd - || exit 1
+"$HOME"/.local/bin/chezmoi init --apply git@github.com:arthurnunesc/dotfiles.git
