744b40c76445a4107051a8048369dcf7dce71c9f
Arthur Nunes <arthurnunesc@proton.me>
2023-04-28 23:06:32 +0200

    Change from dash to sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 printf "\t- changing remote configurations to make downloads faster...\n"
-dash ./components/fedora.sh
-dash ./components/install_apps.sh
+sh ./components/fedora.sh
+sh ./components/install_apps.sh
